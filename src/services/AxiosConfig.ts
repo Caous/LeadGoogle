8abd7c3a62ec8defad8e7e188c1f6abf82379474
Caous <caous.g@gmail.com>
2025-03-23 21:28:52 -0300

    alter router

--- src/services/AxiosConfig.ts
+++ src/services/AxiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const instanceApi = axios.create({
-  baseURL: import.meta.env.PROD ? "https://prestacao-nuvem-api-backoffice.azurewebsites.net/api" : "https://localhost:7233/api",  
+  baseURL: import.meta.env.PROD ? "https://traning-now-api-h9b4a0b0duc6cjaq.eastus2-01.azurewebsites.net/api" : "https://localhost:7233/api",  
 });
 
